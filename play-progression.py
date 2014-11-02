00dd48cc82d055e3a88ca4ad06fe4822af2f4c73
Dylan Steele <dylansteele8@gmail.com>
2014-11-02 08:18:27 -0500

    Stuff

--- play-progression.py
+++ play-progression.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python
 
 import Myo
-import foo
+import MidiSong
 
 class MingusListener(Myo.Listener):
     def __init__(self, poseMap, *args):
         self.poseMap = poseMap
-        self.isOn = False
         for i in xrange(len(args)):
             setattr(self, args[i][0], args[i][1])
         super(MingusListener, self).__init__()
 
     def on_pose(self, myo, timestamp, pose):
         if pose.name in self.poseMap:
-            if pose.name == "fingers_spread" or pose.name == "fist":
-                self.isOn = self.poseMap[pose.name][0][1]
-            if self.isOn:
-                for i in xrange(len(self.poseMap[pose.name])-2):
-                    value = getattr(self, self.poseMap[pose.name][i][0]) + self.poseMap[pose.name][i][1]
-                    setattr(self, self.poseMap[pose.name][i][0], value)
-                    # print self.poseMap[pose.name][i][0], value
+                value = getattr(self, self.poseMap[pose.name][0][0]) + self.poseMap[pose.name][0][1]
+                setattr(self, self.poseMap[pose.name][0][0], value)
+                print self.poseMap[pose.name][0][0], value
         super(MingusListener, self).on_pose(myo, timestamp, pose)
-
-    def on_orientation_data(self, myo, timestamp, orientation):
-        super(MingusListener, self).on_orientation_data(myo, timestamp, orientation)
-        if self.isOn:
-            setattr(self, "modulation", abs(self.pitch_w))
 
 from mingus.core import progressions, intervals
 from mingus.core import chords as ch
@@ -36,55 +26,73 @@ from random import random
 
 class MingusHub(Myo.MyoHub):
     def init(self):
-        self.SF2 = "soundfont.sf2"
-        self.progression = ["I", "V"]
-        self.keyMap = {0: 'C', 1: 'C#', 2: 'D', 3: 'D#', 4: 'E', 5: 'F',
-                       6: 'F#', 7: 'G', 8: 'G#', 9: 'A', 10: 'A#', 11: 'B'}
-        self.prevKey = self.keyMap[listener.key % 12]
-        self.chords = progressions.to_chords(self.progression, self.prevKey)
+        self.SF2Index = 0
+        self.SF2List = ["soundfont.sf2", "Analog Age_set.sf2", "HeavyDTWaves.SF2", "jonnypad10.sf2", "space_wings.SF2"]
 
-        if not fluidsynth.init(self.SF2):
-            print "Couldn't load soundfont", self.SF2
+        # self.progression = ["I", "V"]
+        # self.keyMap = {0: 'C', 1: 'C#', 2: 'D', 3: 'D#', 4: 'E', 5: 'F',
+        #                6: 'F#', 7: 'G', 8: 'G#', 9: 'A', 10: 'A#', 11: 'B'}
+        # self.prevKey = self.keyMap[listener.key % 12]
+        # self.chords = progressions.to_chords(self.progression, self.prevKey)
+
+        if not fluidsynth.init(self.SF2List[self.SF2Index%len(self.SF2List)]):
+            print "Couldn't load soundfont", self.SF2List[self.SF2Index%len(self.SF2List)]
             sys.exit(1)
+
+        self.Song = MidiSong.MidiSong('sample_midi/DukeSingleLonger.mid')
+        self.tick = 0.002*listener.tickScale
 
     def run(self):
         self.init()
         self.hub.run(1000, self.listener)
-        #####
-        currentNotes = "SOMETHING"
-        currNoteList = []
+        currState = self.Song.getRandomState(0)
+        stateHistory = [currState]
+        c = Note()
+        previousSFIndex = self.SF2Index
+        asd = 1
         try:
-            # while self.hub.running:
-                nextNotesDict = foo.MidiState()
-                for key in nextNotesDict.state:
-                    note = Note()
-                    note.from_int(key)
-                    note.velocity = nextNotesDict.state[key]
-                    print note
-                    if note in currNoteList:
-                        currNoteList
-                    else:
-                        currNoteList.append(note)
-                print currNoteList
+            while self.hub.running:
+                print asd
+                asd = (asd + 10)%127 + 1
+                fluidsynth.modulation(1, asd)
+                self.SF2Index = listener.SF2Index
+                print self.SF2Index, previousSFIndex
+                if previousSFIndex != self.SF2Index:
+                    if not fluidsynth.init(self.SF2List[self.SF2Index%len(self.SF2List)]):
+                        print "Couldn't load soundfont", self.SF2List[self.SF2Index%len(self.SF2List)]
+                        sys.exit(1)
+                    previousSFIndex = self.SF2Index
+                state = self.Song.getRandomNextState(0, tuple(stateHistory))
 
-                for note in currNoteList:
-                    fluidsynth.play_Note(note, 1, note.velocity)
+                if state != currState:
+                    missingNotes, extraPitches = currState.getDiff(state)
+                    for note in missingNotes:
+                        fluidsynth.play_Note(c.from_int(note[0]), 1, note[1]*listener.volumeScale)
+                    for pitch in extraPitches:
+                        fluidsynth.stop_Note(c.from_int(pitch), 1)
+                    currState = state
+
+                stateHistory.append(currState)
+
+                if len(stateHistory) > MidiSong.MidiSong.maxOrder:
+                    stateHistory = stateHistory[-MidiSong.MidiSong.maxOrder:]
+
+                time.sleep(self.tick*self.Song.tracks[0]['width'])
 
         except KeyboardInterrupt:
             print "Quitting ..."
             self.hub.stop(True)
 
+poseMap = {'wave_in': [('volumeScale', -0.05)],
+           'wave_out': [('volumeScale', +0.05)],
+           'fist': [('SF2Index', -1),
+                    ('tickScale', -1)],
+           'fingers_spread': [('SF2Index', +1),
+                              ('tickScale', +1)]}
 
-poseMap = {'wave_in': [('baseRandom', -0.1), ('key', 1)],
-           'wave_out': [('baseRandom', 0.1), ('key', -1)],
-           'fist': [('isOn', False)],
-           'fingers_spread': [('isOn', True)]}
-
-listener = MingusListener(poseMap, ('baseRandom', 0.5),
-                                   ('ninthRandom', 0.5),
-                                   ('highRandom', 0.5),
-                                   ('key', 1),
-                                   ('modulation', 0))
+listener = MingusListener(poseMap, ('volumeScale', 1),
+                                   ('tickScale', 1),
+                                   ('SF2Index', 0))
 hub = MingusHub(listener)
 
 hub.run()
